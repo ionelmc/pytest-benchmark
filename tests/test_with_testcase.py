d8035c3f29c4391f58ccb55acadb42805f8c69df
Ionel Cristian Mărieș <contact@ionelmc.ro>
2015-02-06 16:06:08 +0200

    Add examples with unittest.TestCase.

---
+++ tests/test_with_testcase.py
@@ -0,0 +1,23 @@
+import time
+import unittest
+
+import pytest
+
+
+class TerribleTerribleWayToWriteTests(unittest.TestCase):
+    @pytest.fixture(autouse=True)
+    def setupBenchmark(self, benchmark):
+        self.benchmark = benchmark
+
+    def test_foo(self):
+        self.benchmark(time.sleep, 0.000001)
+
+
+class TerribleTerribleWayToWritePatchTests(unittest.TestCase):
+    @pytest.fixture(autouse=True)
+    def setupBenchmark(self, benchmark_weave):
+        self.benchmark_weave = benchmark_weave
+
+    def test_foo2(self):
+        with self.benchmark_weave('time.sleep'):
+            time.sleep(0.0000001)
