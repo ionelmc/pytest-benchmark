68de54b4a1ddc29ba516196b3184a8809dfcaed1
Ionel Cristian Mărieș <contact@ionelmc.ro>
2015-06-15 00:45:06 +0300

    Add a sample test.

---
+++ tests/test_calibration.py
@@ -0,0 +1,25 @@
+from functools import partial
+import time
+
+import pytest
+
+
+def slow_warmup():
+    x = 0
+    for _ in range(1000):
+        x *= 1
+
+
+@pytest.mark.benchmark(warmup=True, warmup_iterations=10 ** 8, max_time=10)
+def test_calibrate(benchmark):
+    benchmark(slow_warmup)
+
+
+@pytest.mark.benchmark(warmup=True, warmup_iterations=10 ** 8, max_time=10)
+def test_calibrate_fast(benchmark):
+    benchmark(lambda: [int] * 100)
+
+
+@pytest.mark.benchmark(warmup=True, warmup_iterations=10 ** 8, max_time=10)
+def test_calibrate_slow(benchmark):
+    benchmark(partial(time.sleep, 0.00001))
