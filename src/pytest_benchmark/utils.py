0daabb836c8868e442605364cef74561df868024
Ionel Cristian Mărieș <contact@ionelmc.ro>
2015-08-10 13:14:42 +0300

    Only try to clone objects with __code__.

--- src/pytest_benchmark/utils.py
+++ src/pytest_benchmark/utils.py
@@ -188,12 +188,15 @@ def clonefunc(f):
 
     from: https://bitbucket.org/antocuni/pypytools/src/tip/pypytools/util.py?at=default
     """
+
     # first of all, we clone the code object
-    co = f.__code__
-    co2 = types.CodeType(co.co_argcount, co.co_nlocals, co.co_stacksize, co.co_flags, co.co_code,
-                         co.co_consts, co.co_names, co.co_varnames, co.co_filename, co.co_name,
-                         co.co_firstlineno, co.co_lnotab, co.co_freevars, co.co_cellvars)
-    #
-    # then, we clone the function itself, using the new co2
-    f2 = types.FunctionType(co2, f.__globals__, f.__name__, f.__defaults__, f.__closure__)
-    return f2
+    if hasattr(f, '__code__'):
+        co = f.__code__
+        co2 = types.CodeType(co.co_argcount, co.co_nlocals, co.co_stacksize, co.co_flags, co.co_code,
+                             co.co_consts, co.co_names, co.co_varnames, co.co_filename, co.co_name,
+                             co.co_firstlineno, co.co_lnotab, co.co_freevars, co.co_cellvars)
+        #
+        # then, we clone the function itself, using the new co2
+        return types.FunctionType(co2, f.__globals__, f.__name__, f.__defaults__, f.__closure__)
+    else:
+        return f
