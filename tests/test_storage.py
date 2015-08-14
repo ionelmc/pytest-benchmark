5b78858eb718649a31fb93d8dc96ca2cee41a4cd
Ionel Cristian Mărieș <contact@ionelmc.ro>
2015-08-15 02:14:06 +0300

    Update tests.

--- tests/test_storage.py
+++ tests/test_storage.py
@@ -20,6 +20,8 @@ SAVE_DATA = json.load(STORAGE.listdir('0030_*.json')[0].open())
 SAVE_DATA["benchmarks"][0]["stats"]["include_data"] = False
 JSON_DATA = json.load(STORAGE.listdir('0030_*.json')[0].open())
 JSON_DATA["benchmarks"][0]["stats"]["include_data"] = True
+SAVE_DATA["machine_info"] = JSON_DATA["machine_info"] = {'foo': 'bar'}
+SAVE_DATA["commit_info"] = JSON_DATA["commit_info"] = {'foo': 'bar'}
 
 
 class Namespace(object):
@@ -122,33 +124,58 @@ def test_rendering(sess):
 def test_regression_checks(sess):
     output = make_logger(sess)
     sess.handle_loading()
-    sess.compare == '0001'
     sess.performance_regressions = []
     sess.compare_fail = [
         PercentageRegressionCheck("stddev", 5),
-        DifferenceRegressionCheck("max", 0.5)
+        DifferenceRegressionCheck("max", 0.000001)
     ]
     sess.display_results_table(Namespace(
         write_line=lambda line, **opts: output.write(force_text(line) + u'\n'),
         write=lambda text, **opts: output.write(force_text(text)),
     ))
+    print(output.getvalue())
     assert sess.performance_regressions == [
-        ('tests/test_func/test_perf.py::test_engine',
-         'Field stddev has failed PercentageRegressionCheck: 26.572963937 > '
-         '5.000000000'),
-        ('tests/test_func/test_perf.py::test_engine',
-         'Field max has failed DifferenceRegressionCheck: 0.617182970 > 0.500000000')
+        ('tests/test_normal.py::test_xfast_parametrized[0]',
+         'Field stddev has failed PercentageRegressionCheck: 75.093832852 > 5.000000000'),
+        ('tests/test_normal.py::test_xfast_parametrized[0]',
+         'Field max has failed DifferenceRegressionCheck: 0.000001058 > 0.000001000')
     ]
     output = make_logger(sess)
     pytest.raises(PerformanceRegression, sess.check_regressions)
     print(output.getvalue())
     assert output.getvalue() == """Performance has regressed:
-\ttests/test_func/test_perf.py::test_engine - Field stddev has failed PercentageRegressionCheck: 26.572963937 \
-> 5.000000000
-\ttests/test_func/test_perf.py::test_engine - Field max has failed DifferenceRegressionCheck: 0.617182970 > \
-0.500000000
+\ttests/test_normal.py::test_xfast_parametrized[0] - Field stddev has failed PercentageRegressionCheck: 75.093832852 > 5.000000000
+\ttests/test_normal.py::test_xfast_parametrized[0] - Field max has failed DifferenceRegressionCheck: 0.000001058 > 0.000001000
 """
 
+
+def test_regression_checks_inf(sess):
+    output = make_logger(sess)
+    sess.compare = '0002'
+    sess.handle_loading()
+    sess.performance_regressions = []
+    sess.compare_fail = [
+        PercentageRegressionCheck("stddev", 5),
+        DifferenceRegressionCheck("max", 0.000001)
+    ]
+    sess.display_results_table(Namespace(
+        write_line=lambda line, **opts: output.write(force_text(line) + u'\n'),
+        write=lambda text, **opts: output.write(force_text(text)),
+    ))
+    print(output.getvalue())
+    assert sess.performance_regressions == [
+        ('tests/test_normal.py::test_xfast_parametrized[0]',
+         'Field stddev has failed PercentageRegressionCheck: inf > 5.000000000'),
+        ('tests/test_normal.py::test_xfast_parametrized[0]',
+         'Field max has failed DifferenceRegressionCheck: 0.000001058 > 0.000001000')
+    ]
+    output = make_logger(sess)
+    pytest.raises(PerformanceRegression, sess.check_regressions)
+    print(output.getvalue())
+    assert output.getvalue() == """Performance has regressed:
+\ttests/test_normal.py::test_xfast_parametrized[0] - Field stddev has failed PercentageRegressionCheck: inf > 5.000000000
+\ttests/test_normal.py::test_xfast_parametrized[0] - Field max has failed DifferenceRegressionCheck: 0.000001058 > 0.000001000
+"""
 
 
 def test_compare(sess):
@@ -160,22 +187,47 @@ def test_compare(sess):
     ))
     print(output.getvalue())
     assert output.getvalue() == """Benchmark machine_info is different. Current: {foo: "bar"} VS saved: {machine: "x86_64", node: "minibox", processor: "x86_64", python_compiler: "GCC 4.6.3", python_implementation: "CPython", python_version: "2.7.3", release: "3.13.0-55-generic", system: "Linux"}.
-Comparing against benchmark 0001_001f2d07786d4f90b406372dba594658a43a2558_20150814_181653_uncommitted-changes.json:
-| commit info: {dirty: true, id: "001f2d07786d4f90b406372dba594658a43a2558"}
-| saved at: 2015-08-14T15:16:53.144088
+Comparing against benchmark 0001_b87b9aae14ff14a7887a6bbaa9731b9a8760555d_20150814_190343_uncommitted-changes.json:
+| commit info: {dirty: true, id: "b87b9aae14ff14a7887a6bbaa9731b9a8760555d"}
+| saved at: 2015-08-14T16:03:43.699338
 | saved using pytest-benchmark 2.5.0:
-------------------------------------------- benchmark: 1 tests, min 123 rounds (of min 234), 345 max time, timer: None -------------------------------------------
-Name (time in ns)                  Min              Max             Mean          StdDev           Median             IQR          Outliers(*)  Rounds  Iterations
-------------------------------------------------------------------------------------------------------------------------------------------------------------------
-test_xfast_parametrized[0]     72.9561          82.9697          75.0167          3.2819          73.9098          2.1458                  2;2      14        1000
-                                    NC          -2.1458 (2%)     -0.3406 (0%)    -0.2748 (7%)          NC         +1.9073 (800%)           2;4      14        1000
-------------------------------------------------------------------------------------------------------------------------------------------------------------------
+----------------------------------------------------------- benchmark: 1 tests, min 123 rounds (of min 234), 345 max time, timer: None -----------------------------------------------------------
+Name (time in ns)                   Min                     Max                  Mean                StdDev                Median                 IQR              Outliers(*)  Rounds  Iterations
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+test_xfast_parametrized[0]     217.3145              3,409.1575              249.0662              139.4791              220.1664              2.2815                 329;2506   10755         418
+                                +1.0744 (0%)        +1,057.6850 (44%)        +15.3444 (6%)         +59.8195 (75%)         +1.7085 (0%)        +1.7271 (311%)            90;923    2653         430
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 (*) Outliers: 1 Standard Deviation from Mean; 1.5 IQR (InterQuartile Range) from 1st Quartile and 3rd Quartile.
 
 """
 
 
-@freeze_time("2012-01-14 12:00:01")
+def test_compare_2(sess):
+    output = make_logger(sess)
+    sess.compare = '0002'
+    sess.handle_loading()
+    sess.display_results_table(Namespace(
+        write_line=lambda line, **opts: output.write(force_text(line) + u'\n'),
+        write=lambda text, **opts: output.write(force_text(text)),
+    ))
+    print(output.getvalue())
+    assert output.getvalue() == """Benchmark machine_info is different. Current: {foo: "bar"} VS saved: {machine: "x86_64", node: "minibox", processor: "x86_64", python_compiler: "GCC 4.6.3", python_implementation: "CPython", python_version: "2.7.3", release: "3.13.0-55-generic", system: "Linux"}.
+Comparing against benchmark 0002_b87b9aae14ff14a7887a6bbaa9731b9a8760555d_20150814_190348_uncommitted-changes.json:
+| commit info: {dirty: true, id: "b87b9aae14ff14a7887a6bbaa9731b9a8760555d"}
+| saved at: 2015-08-14T16:03:48.342017
+| saved using pytest-benchmark 2.5.0:
+----------------------------------------------------------- benchmark: 1 tests, min 123 rounds (of min 234), 345 max time, timer: None -----------------------------------------------------------
+Name (time in ns)                   Min                     Max                  Mean                StdDev                Median                 IQR              Outliers(*)  Rounds  Iterations
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+test_xfast_parametrized[0]     217.3145              3,409.1575              249.0662              139.4791              220.1664              2.2815                 329;2506   10755         418
+                                -6.6908 (2%)        +1,057.6850 (44%)        +11.6857 (4%)        +139.4791 (infinite%)   -6.8417 (3%)        +2.2815 (infinite%)     329;1985   10980         397
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+(*) Outliers: 1 Standard Deviation from Mean; 1.5 IQR (InterQuartile Range) from 1st Quartile and 3rd Quartile.
+
+"""
+
+
+@freeze_time("2015-08-14T16:06:07.146833")
 def test_save_json(sess, tmpdir):
     sess.save = False
     sess.autosave = False
@@ -186,7 +238,7 @@ def test_save_json(sess, tmpdir):
     assert json.loads(sess.json.getvalue().decode()) == JSON_DATA
 
 
-@freeze_time("2012-01-14 12:00:01")
+@freeze_time("2015-08-14T16:06:07.146833")
 def test_save_with_name(sess, tmpdir):
     sess.save = 'foobar'
     sess.autosave = True
@@ -199,7 +251,7 @@ def test_save_with_name(sess, tmpdir):
     assert json.load(files[0].open('rU')) == SAVE_DATA
 
 
-@freeze_time("2012-01-14 12:00:01")
+@freeze_time("2015-08-14T16:06:07.146833")
 def test_save_no_name(sess, tmpdir):
     sess.save = True
     sess.autosave = True
@@ -212,7 +264,7 @@ def test_save_no_name(sess, tmpdir):
     assert json.load(files[0].open('rU')) == SAVE_DATA
 
 
-@freeze_time("2012-01-14 12:00:01")
+@freeze_time("2015-08-14T16:06:07.146833")
 def test_autosave(sess, tmpdir):
     sess.save = False
     sess.autosave = True
