db7b9fafef5ee54361e82347deeb8675388aeb84
Ionel Cristian Mărieș <contact@ionelmc.ro>
2015-08-31 11:06:28 +0300

    More updates from cookiecutter-pylibrary.

--- setup.py
+++ setup.py
@@ -1,15 +1,19 @@
+#!/usr/bin/env python
 # -*- encoding: utf-8 -*-
-import glob
+from __future__ import absolute_import, print_function
+
 import io
+import os
 import re
+from glob import glob
 from os.path import basename
 from os.path import dirname
 from os.path import join
+from os.path import relpath
 from os.path import splitext
 
 from setuptools import find_packages
 from setuptools import setup
-
 
 def read(*names, **kwargs):
     return io.open(
@@ -17,18 +21,19 @@ def read(*names, **kwargs):
         encoding=kwargs.get('encoding', 'utf8')
     ).read()
 
+
 setup(
     name='pytest-benchmark',
     version='2.5.0',
     license='BSD',
-    description='py.test fixture for benchmarking code',
-    long_description='%s\n%s' % (read('README.rst'), re.sub(':obj:`~?(.*?)`', r'``\1``', read('CHANGELOG.rst'))),
+    description='A ``py.test`` fixture for benchmarking code.',
+    long_description='%s\n%s' % (read('README.rst'), re.sub(':[a-z]+:`~?(.*?)`', r'``\1``', read('CHANGELOG.rst'))),
     author='Ionel Cristian Mărieș',
     author_email='contact@ionelmc.ro',
     url='https://github.com/ionelmc/pytest-benchmark',
     packages=find_packages('src'),
     package_dir={'': 'src'},
-    py_modules=[splitext(basename(i))[0] for i in glob.glob('src/*.py')],
+    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
     include_package_data=True,
     zip_safe=False,
     classifiers=[
