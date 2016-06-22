347ef71816c12db37b71d8fd75c699b93a066559
Ionel Cristian Mărieș <contact@ionelmc.ro>
2016-06-22 13:14:17 +0300

    Add few classifiers.

--- setup.py
+++ setup.py
@@ -37,11 +37,12 @@ setup(
     classifiers=[
         # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
         'Development Status :: 5 - Production/Stable',
+        'Framework :: Pytest',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
-        'Operating System :: Unix',
-        'Operating System :: POSIX',
         'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Operating System :: Unix',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
@@ -51,6 +52,8 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Software Development :: Testing',
+        'Topic :: System :: Benchmark',
         'Topic :: Utilities',
     ],
     keywords=[
