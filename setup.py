719867482314eb0e34c66577745ebd4d2c810a70
Ionel Cristian Mărieș <contact@ionelmc.ro>
2016-10-09 15:08:09 +0300

    Formatting.

--- setup.py
+++ setup.py
@@ -26,7 +26,9 @@ setup(
     name='pytest-benchmark',
     version='3.0.0',
     license='BSD',
-    description='A ``py.test`` fixture for benchmarking code. It will group the tests into rounds that are calibrated to the chosen timer. See calibration_ and FAQ_.',
+    description='A ``py.test`` fixture for benchmarking code. '
+                'It will group the tests into rounds that are calibrated to the chosen timer. '
+                'See calibration_ and FAQ_.',
     long_description='%s\n%s' % (
         re.compile('^.. start-badges.*^.. end-badges', re.M | re.S).sub('', read('README.rst')),
         re.sub(':[a-z]+:`~?(.*?)`', r'``\1``', read('CHANGELOG.rst'))
