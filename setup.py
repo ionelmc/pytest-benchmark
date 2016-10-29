75e5eef383695fe6862cc3a503a58bc40eefd1b3
Ionel Cristian Mărieș <contact@ionelmc.ro>
2016-10-29 20:09:28 +0300

    Bump version: 3.0.0 → 3.1.0a1

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def read(*names, **kwargs):
 
 setup(
     name='pytest-benchmark',
-    version='3.0.0',
+    version='3.1.0a1',
     license='BSD',
     description='A ``py.test`` fixture for benchmarking code. '
                 'It will group the tests into rounds that are calibrated to the chosen timer. '
