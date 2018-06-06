810459cd2de55cca75091f848ee502ea3ddce848
Ionel Cristian Mărieș <contact@ionelmc.ro>
2018-06-06 12:44:48 +0300

    Reformat.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -181,14 +181,15 @@ def test_get_project_name_broken(scm, testdir):
 
 def test_parse_elasticsearch_storage():
     assert parse_elasticsearch_storage("http://localhost:9200") == (
-    ["http://localhost:9200"], "benchmark", "benchmark", "pytest-benchmark")
+        ["http://localhost:9200"], "benchmark", "benchmark", "pytest-benchmark")
     assert parse_elasticsearch_storage("http://localhost:9200/benchmark2") == (
-    ["http://localhost:9200"], "benchmark2", "benchmark", "pytest-benchmark")
+        ["http://localhost:9200"], "benchmark2", "benchmark", "pytest-benchmark")
     assert parse_elasticsearch_storage("http://localhost:9200/benchmark2/benchmark2") == (
-    ["http://localhost:9200"], "benchmark2", "benchmark2", "pytest-benchmark")
+        ["http://localhost:9200"], "benchmark2", "benchmark2", "pytest-benchmark")
     assert parse_elasticsearch_storage("http://host1:9200,host2:9200") == (
-    ["http://host1:9200", "http://host2:9200"], "benchmark", "benchmark", "pytest-benchmark")
+        ["http://host1:9200", "http://host2:9200"], "benchmark", "benchmark", "pytest-benchmark")
     assert parse_elasticsearch_storage("http://host1:9200,host2:9200/benchmark2") == (
-    ["http://host1:9200", "http://host2:9200"], "benchmark2", "benchmark", "pytest-benchmark")
+        ["http://host1:9200", "http://host2:9200"], "benchmark2", "benchmark", "pytest-benchmark")
     assert parse_elasticsearch_storage("http://localhost:9200/benchmark2/benchmark2?project_name=project_name") == (
-    ["http://localhost:9200"], "benchmark2", "benchmark2", "project_name")
+        ["http://localhost:9200"], "benchmark2", "benchmark2", "project_name")
+
