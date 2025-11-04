3670df1cbfdc6051ba693ee0f1af0a66423a9f51
Ionel Cristian Mărieș <contact@ionelmc.ro>
2025-11-04 16:49:21 +0200

    Bump version: 5.2.0 → 5.2.1

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@ project = 'pytest-benchmark'
 year = '2014-2025'
 author = 'Ionel Cristian Mărieș'
 copyright = f'{year}, {author}'
-version = release = '5.2.0'
+version = release = '5.2.1'
 
 pygments_style = 'trac'
 templates_path = ['.']
