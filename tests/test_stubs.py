1b3e542c893acaaeac4b5762a40716a55e5d50c9
hwelch-fle <hwelch@finelines-engineering.com>
2026-05-24 18:23:52 -0400

    Add stub tests

---
+++ tests/test_stubs.py
@@ -0,0 +1,53 @@
+from typing import Any, assert_type
+import cProfile
+
+from src.pytest_benchmark.fixture import BenchmarkFixture
+
+import pytest
+
+pytest.mark.skip('No Run (Type Check)')
+def test_benchmarkfixture_hints(benchmark: BenchmarkFixture):
+
+    # Attributes
+    assert_type(benchmark.name, str)
+    assert_type(benchmark.fullname, str)
+    assert_type(benchmark.disabled, bool)
+    assert_type(benchmark.param, str | None)
+    assert_type(benchmark.params, tuple[str, ...] | None)
+    assert_type(benchmark.group, str | None)
+    assert_type(benchmark.has_error, bool)
+    assert_type(benchmark.extra_info, dict[str, Any])
+    assert_type(benchmark.skipped, bool)
+    assert_type(benchmark.cprofile, cProfile.Profile)
+    assert_type(benchmark.cprofile_loops, int | None)
+    assert_type(benchmark.cprofile_dump, str | None)
+    assert_type(benchmark.cprofile_stats, dict[Any, Any] | None)
+    assert_type(benchmark.stats, dict[Any, Any] | None)
+
+    # Properties
+    assert_type(benchmark.enabled, bool)
+
+    # Methods
+
+    # Test that types are maintained when __call__ ing the fixture
+    assert_type(benchmark.__call__(map, len, 'a'), map[int])
+    assert_type(benchmark.__call__(len, []), int)
+    assert_type(benchmark.__call__(repr, []), str)
+
+    # `args`/`kwargs` 
+    assert_type(benchmark.pedantic(len, args=([],)), int)
+    assert_type(benchmark.pedantic(len, kwargs={}), int)
+    assert_type(benchmark.pedantic(len, args=tuple(), kwargs={}), int)
+    assert_type(benchmark.pedantic(len, tuple(), {}), int)
+    assert_type(benchmark.pedantic(len, kwargs={}, rounds=10, teardown=print), int)
+
+    # `setup`
+    assert_type(benchmark.pedantic(len, setup=lambda: (tuple(), {})), int)
+    assert_type(benchmark.pedantic(len, setup=lambda: (([1,2,3],), {})), int)
+
+
+    # Reverse Positionals (Should show Type Error)
+    benchmark.pedantic(len, {}, tuple())
+
+    # Pass `args`/`kwargs` and `setup` (Should show Type Error)
+    benchmark.pedantic(len, args=tuple([]), setup=lambda: (tuple(), dict()))
